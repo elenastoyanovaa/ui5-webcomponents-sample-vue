28e1e2a03eca4d13ef331f7ca33effe2e7974efa
elenastoyanovaa <elenastoyanovaaa@gmail.com>
2024-08-28 11:20:19 +0300

    chore: update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,8 @@
 <template>
-  <ui5-list id="todo-list" mode="MultiSelect" ref="list">
-    <TodoItem v-for="(todo) in todos" :todo="todo" :key="todo.id" :datakey="todo.id"
-    @item-deleted="onItemDeleted"
-    @item-edit="onItemEdit">
-    </TodoItem>
-  </ui5-list>
+    <ui5-date-picker></ui5-date-picker>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue';
 import "@ui5/webcomponents/dist/List";
 
 
